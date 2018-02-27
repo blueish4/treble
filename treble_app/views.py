07e44b225baea5ec93e8b750b5939f056cbf37dc
Miklas Riechmann <2247469r@student.gla.ac.uk>
2018-02-27 19:05:05 +0000

    URLs and placeholder views set up

--- treble_app/views.py
+++ treble_app/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+# No need to import once all views return render()
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('index')
+
+def user_login(request):
+    return HttpResponse('login')
+
+def register(request):
+    return HttpResponse('register')
+
+def user_profile(request, username_slug):
+    return HttpResponse('profile: ' + username_slug)
+
+def user_account(request, username_slug):
+    return HttpResponse('account: ' + username_slug)
+
+def song(request, song_name_slug):
+    return HttpResponse('song: ' + song_name_slug)
+
+def song_comment(request, song_name_slug):
+    return HttpResponse('comment: ' + song_name_slug)
+
+def song_recommendation(request, song_name_slug):
+    return HttpResponse('recommendation: ' + song_name_slug)
+
+def about(request):
+    return HttpResponse('about')
+
+def contact(request):
+    return HttpResponse('contact')
+
+def faq(request):
+    return HttpResponse('faq')
+
+def user_logout(request):
+    return HttpResponse('logout')
