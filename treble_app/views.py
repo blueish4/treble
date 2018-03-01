51128bdbcb98e7e1546de7dc38308f05e45e89eb
Miklas Riechmann <2247469r@student.gla.ac.uk>
2018-03-01 17:28:24 +0000

    Completed views and ammended URLs

--- treble_app/views.py
+++ treble_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from treble_app.models import Song
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-# No need to import HttpResponse once all views return render()
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect # No need to import HttpResponse once all views return render()
 from django.core.urlresolvers import reverse
+from treble.forms import UserForm, UserProfileForm
+from treble_app.models import Song
 
 def index(request):
     most_recommended_songs = Song.objects.order_by('-recommendations')[:5]
@@ -36,17 +37,58 @@ def user_login(request):
             return render(request, 'treble/login.html', {'message':message})
     else:
         message = ''
-        return render(request, 'rango/login.html', {'message':message})
-    return HttpResponse('login')
+        return render(request, 'treble/login.html', {'message':message})
 
 def register(request):
-    return HttpResponse('register')
+    # True if registration was successful
+    registered = False
 
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            # Hash password and update user object
+            user.set_password(user.password)
+            user.save()
+
+            # commit=False delays saving model, as user attributes must still be set
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            # Profile pic. supplied?
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+
+            # Template registration was successful
+            registered = True
+
+        else:
+            # Invalid form / mistakes
+            print(user_form.errors, profile_form.errors)
+
+    else:
+        # Not HTTP POST so render blank form
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'treble/register.html', {'user_form': user_form,
+                                                   'profile_form': profile_form,
+                                                   'registered': registered})
+                                                   
+@login_required # Can only view other profiles if user is logged in
 def user_profile(request, username_slug):
-    return HttpResponse('profile: ' + username_slug)
+    user = User.objects.get(username=username_slug)
+    return render(request, 'treble/user_profile.html', user)
 
-def user_account(request, username_slug):
-    return HttpResponse('account: ' + username_slug)
+@login_required
+def user_account(request):
+    user = request.user
+    return render(request, 'treble/user_account.html', user)
 
 def song(request, song_name_slug):
     context_dict = {}
@@ -58,7 +100,7 @@ def song(request, song_name_slug):
     except Song.DoesNotExist:
         context_dict['song'] = None
 
-    return render(request, 'rango/song.html', context_dict)
+    return render(request, 'treble/song.html', context_dict)
 
 @login_required
 def add_song(request):
@@ -71,7 +113,7 @@ def add_song(request):
         print(form.errors)
 
     # Handles bad form, new form, or no form supplied cases and provides error message
-    return render(request,'rango/add_song.html', {'form':form})
+    return render(request,'treble/add_song.html', {'form':form})
 
 def song_comment(request, song_name_slug):
     return HttpResponse('comment: ' + song_name_slug)
@@ -80,14 +122,14 @@ def song_recommendation(request, song_name_slug):
     return HttpResponse('recommendation: ' + song_name_slug)
 
 def about(request):
-    context_dict = {}
-    return render(request, 'treble/about.html', context_dict)
+    return render(request, 'treble/about.html', {})
 
 def contact(request):
-    return HttpResponse('contact')
+    return render(request, 'treble/contact.html', {})
 
 def faq(request):
-    return HttpResponse('faq')
+    return render(request, 'treble/faq.html', {})
 
 def user_logout(request):
-    return HttpResponse('logout')
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
