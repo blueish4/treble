6ed7137b14f02e041a82d75083e3c3388e4d6a3c
Miklas Riechmann <2247469r@student.gla.ac.uk>
2018-03-06 17:41:32 +0000

    Minor fixes

--- treble_app/views.py
+++ treble_app/views.py
@@ -102,40 +102,6 @@ def user_account(request):
 
 def song(request, song_id):
     context_dict = {}
-    # context_dict = {"name": song_name_slug,
-    #                "artist": "Taylor Swift",
-    #                "img": "https://i.scdn.co/image/abd96549fa53000a633d64cbab5a69a623b6bdfa",
-    #                "spotify": "spotify:track:4vVb2D6RYL669h7tLYOKwx",
-    #                "similar": [{"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "/treble/song/this-song-here/"},
-    #                            {"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "/treble/song/this-song-here/"},
-    #                            {"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "/treble/song/this-song-here/"},
-    #                            {"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "/treble/song/this-song-here/"},
-    #                            {"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "https://127.0.0.1:8000/treble/song/this-song-here/"},
-    #                            {"image": "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
-    #                             "name": "This song here",
-    #                             "url": "/treble/song/this-song-here/"}
-    #                            ],
-    #                "reviews": [{"reviewer_name": "Alice",
-    #                             "reaction": ":D",
-    #                             "review_text": "This is a really great song!"},
-    #                            {"reviewer_name": "Alice",
-    #                             "reaction": ":D",
-    #                             "review_text": "This is a really great song!"},
-    #                            {"reviewer_name": "Bob",
-    #                             "reaction": ":(",
-    #                             "review_text": "I am a massive fucking killjoy."}
-    #                            ]
-    #                }
     try:
         song = Song.objects.get(slug=song_id)
         comments = Comment.Objects.get(slug=song_id)
