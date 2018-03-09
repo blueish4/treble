22015025c815433c766099b1d88244b207c55642
Ross Gardiner <2190583g@student.gla.ac.uk>
2018-03-09 15:50:44 +0000

    changed settings.py

--- treble/settings.py
+++ treble/settings.py
@@ -113,7 +113,17 @@ PASSWORD_HASHERS = (
 	'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 	'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',)
 
-
+# If True, users can register
+REGISTRATION_OPEN = True
+# One-week activation window; you may, of course, use a different value.
+ACCOUNT_ACTIVATION_DAYS = 7
+# If True, the user will be automatically logged in.
+REGISTRATION_AUTO_LOGIN = True
+# The page you want users to arrive at after they successfully log in
+LOGIN_REDIRECT_URL = '/rango/'
+# The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+LOGIN_URL = '/accounts/login/'
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
