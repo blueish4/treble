023fec057a7e4aa990728f957069a322276c72b2
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-03-18 14:23:35 +0000

    Added artwork to navbar search and song details to user profile comments

--- templates/treble/navbar_search.js
+++ templates/treble/navbar_search.js
@@ -5,7 +5,7 @@ $(function() {
         this.widget().menu("option", "items", "> :not(.ui-autocomplete-category)");
     },
     _renderItemSong: function(ul, item){
-        var newHtml = "<div class='ui-menu-item-wrapper'><div class='search-menu-song'>" + item.track_name + "</div><div class='search-menu-artist'>by " + item.artist + "</div></div>";
+        var newHtml = "<div class='ui-menu-item-wrapper'><div><img src='" + item.artwork_url +  "'</></div><div class='search-menu-song'>" + item.track_name + "</div><div class='search-menu-artist'>by " + item.artist + "</div></div>";
         return $("<li>").data("ui-autocomplete-item",{"label":item.track_name,"value":item.track_name,"link":"{% url 'song' song_id=0 %}".replace(/0/,item.song_id)}).append(newHtml).appendTo(ul);
     },
     _renderItemUser: function(ul, item){
@@ -16,14 +16,22 @@ $(function() {
         var newHtml = "<div class='ui-menu-item-wrapper'><div class='search-menu-not-logged-in'>" + label + "</div></div>";
         return $("<li>").data("ui-autocomplete-item",{"link":"{% url 'auth_login' %}"}).append(newHtml).appendTo(ul);
     },
+    _renderAddSong: function(ul){
+      var text = "Can't find the song you're looking for? <br/>Add from Spotify by clicking here.";
+      var newHtml = "<div class='search-menu-not-logged-in'>" + text + "</div>";
+      return $("<li>").data("ui-autocomplete-item",{"link":"{% url 'search' search_term=''%}"}).append(newHtml).appendTo(ul);
+    },
     _renderMenu: function(ul, items) {
         var that = this;
         currentCategory = "";
+
         $.each(items, function(index, item) {
             var li;
+
             if (item.category != currentCategory) {
                 ul.append("<li class='ui-autocomplete-category'>" + item.category + "</li>");
                 currentCategory = item.category;
+
             }
 
             if (item.category) {
@@ -32,6 +40,7 @@ $(function() {
                         li = that._renderItemSong(ul, value);
                     })
                 } else {
+
                     if (item.logged_in){
                         $.each(item.label,function(key,value){
                             li = that._renderItemUser(ul, value);
@@ -42,7 +51,13 @@ $(function() {
                     }
                 }
             }
+            if (index < items.length - 1){
+              if (items[index+1].category == "User"){
+                li = that._renderAddSong(ul);
+              }
+            }
         });
+
     }
   });
   $("#search").catcomplete({
