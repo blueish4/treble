64afbc8f8983032fdec35991578056ec446aac18
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-03-21 09:56:09 +0000

    Updates

--- templates/treble/recommend_by_song.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".dropdown-content a").click(function(){
-        var songName = this.innerHTML;
-        document.getElementById("drop").innerText = songName;
-        });
-});
