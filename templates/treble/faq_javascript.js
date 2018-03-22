5408b5403a61001eb0345e7d8901fa7502efc547
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-03-22 19:52:28 +0000

    FAQ js stuff

---
+++ templates/treble/faq_javascript.js
@@ -0,0 +1,22 @@
+$(function(){
+    $(".info").hide();
+    $(".title").on("click", function(){
+      
+      var end = $(this).find(".title-text").text().slice(-3);
+      var rest_of_text = $(this).find(".title-text").text().slice(0,-3);
+      var replace;
+      if (end == "[+]"){
+        replace = rest_of_text + "[-]";
+      }
+      else{
+        replace = rest_of_text + "[+]"
+      }
+      $(this).find(".title-text").text(replace);
+
+      $(this).find('.info').toggle('slide', {
+        duration: 1000,
+        direction: 'up'
+      });
+
+    });
+})
