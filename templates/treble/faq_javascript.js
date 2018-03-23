5a6c2c4bd584342f967b52c37fdccb59bc7f7bbd
Ashwin Maliampurakal <2249314m@student.gla.ac.uk>
2018-03-23 10:55:11 +0000

    Added add_song_form to index context dict

--- templates/treble/faq_javascript.js
+++ templates/treble/faq_javascript.js
@@ -1,22 +1,22 @@
-$(function () {
+$(function(){
     $(".info").hide();
-    $(".title").on("click", function () {
+    $(".title").on("click", function(){
 
-        var end = $(this).find(".title-text").text().slice(-3);
-        var rest_of_text = $(this).find(".title-text").text().slice(0, -3);
-        var replace;
-        if (end === "[+]") {
-            replace = rest_of_text + "[-]";
-        }
-        else {
-            replace = rest_of_text + "[+]"
-        }
-        $(this).find(".title-text").text(replace);
+      var end = $(this).find(".title-text").text().slice(-3);
+      var rest_of_text = $(this).find(".title-text").text().slice(0,-3);
+      var replace;
+      if (end == "[+]"){
+        replace = rest_of_text + "[-]";
+      }
+      else{
+        replace = rest_of_text + "[+]"
+      }
+      $(this).find(".title-text").text(replace);
 
-        $(this).find('.info').toggle('slide', {
-            duration: 1000,
-            direction: 'up'
-        });
+      $(this).find('.info').toggle('slide', {
+        duration: 1000,
+        direction: 'up'
+      });
 
     });
-});
+})
