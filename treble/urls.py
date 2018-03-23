c98013560641079ab3bdac5865ed5ae7ecce9f0a
Michael Hathaway <michael@hathaways.me.uk>
2018-03-23 00:14:55 +0000

    Whitespace and code style fixes for python code

--- treble/urls.py
+++ treble/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from treble_app import views
 
 urlpatterns = [
-    url(r'^$',views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^treble/', include('treble_app.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
