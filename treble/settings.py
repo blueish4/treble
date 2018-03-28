71303906b94f4a98f92863ded04d464d7c39b931
Miklas Riechmann <2247469r@student.gla.ac.uk>
2018-03-28 14:02:35 +0100

    Debug set to false and custom 404 added

--- treble/settings.py
+++ treble/settings.py
@@ -25,9 +25,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'd55t-c8ws-af9h_&yi$*rts!(6=th8df2gf8*3^9r$tltn^1&g'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['treble.pythonanywhere.com', '127.0.0.1']
 
 
