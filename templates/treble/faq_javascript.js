6c963cef5715eaa3cd4ec3bf0b959a063f09493b
Miklas Riechmann <2247469r@student.gla.ac.uk>
2018-03-28 14:02:35 +0100

    Debug set to false and custom 404 added

--- templates/treble/faq_javascript.js
+++ templates/treble/faq_javascript.js
@@ -1,10 +1,6 @@
 $(function(){
     $(".info").hide();
-<<<<<<< HEAD
     $(".title").click( function () {
-=======
-    $(".title").on("click", function(){
->>>>>>> b6280b8584aca870f5cbf58951c3b076232004be
 
       var end = $(this).find(".title-text").text().slice(-3);
       var rest_of_text = $(this).find(".title-text").text().slice(0,-3);
@@ -17,14 +13,6 @@ $(function(){
       }
       $(this).find(".title-text").text(replace);
 
-<<<<<<< HEAD
         $(this).find('.info').slideToggle();
-=======
-      $(this).find('.info').toggle('slide', {
-        duration: 1000,
-        direction: 'up'
-      });
->>>>>>> b6280b8584aca870f5cbf58951c3b076232004be
-
     });
 })
